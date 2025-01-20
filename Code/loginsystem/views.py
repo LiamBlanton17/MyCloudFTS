b992564edac6c09e9b9a2943a2cc599d636160d6
LiamBlanton17 <liamblanton@gmail.com>
2025-01-20 08:19:05 -0800

    Name changed to FTS

--- Code/loginsystem/views.py
+++ Code/loginsystem/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'login.html')
+    name = "My Name"
+    return render(request, 'login.html', {
+        'name': name,
+    })
