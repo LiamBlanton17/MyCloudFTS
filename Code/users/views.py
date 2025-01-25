58c9bdf63478f1909c909d72110f0ec358c0e35c
LiamBlanton17 <liamblanton@gmail.com>
2025-01-25 15:25:35 -0800

    First PR
    Deleted login system.
    Renamed login system to users app.
    Connected backend to work with latest frontend changes from Ivan.
    Redirected blank url to landpage.html.
    Added css link that will allow it to load when Django server is running.

---
+++ Code/users/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.db import connection
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'login.html')
+
+
+# Create your views here.
+def landing_page(request):
+    return render(request, 'landingpage.html')
