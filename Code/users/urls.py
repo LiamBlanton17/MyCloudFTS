fafc7763c4054409e6e08b630f21ff16d047a0f9
ivanchikb360 <ivanchikb360@gmail.com>
2025-02-13 13:55:04 -0800

    add user dash

--- Code/users/urls.py
+++ Code/users/urls.py
@@ -7,10 +7,7 @@ urlpatterns = [
     path('landingpage.html', views.landing_page, name='landingpage'),
     path('signup.html', views.signup, name='signup'),
     path('', RedirectView.as_view(url='landingpage.html', permanent=True)),
-<<<<<<< HEAD
     path('dashboard.html', views.dashboard, name='dashboard'),
-=======
->>>>>>> dadbfc3e1a24f048b88ea261ae4ec7b25c1afac5
     # Below are api POSTS
     path('api/post/sign_up/', views.api_sign_up, name='signup'),
     path('api/post/login/', views.api_login, name='login'),
